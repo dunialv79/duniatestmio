1210245abbb502a38bc2ac63133325ee234557d3
dunialv79 <dunialv79@gmail.com>
2021-03-25 10:51:55 +0100

    con plantilla boostrap incluida

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
-
+from frontend import views
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('', views.index, name="index"),
+    path('products', views.products, name="products"),
+    path('store', views.store, name="store"),
+    path('about', views.about, name="about"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 # Create your views here.
+
 def index(request):
+    return render(request,"frontend/index.html")
 
+def products(request):
+    return render(request,"frontend/products.html")
 
-    context = {
-        'meta_description': "Base del meta"}
-    return render(request, 'frontend/index.html', context)
+def store(request):
+    return render(request,"frontend/store.html")
+
+def about(request):
+    return render(request,"frontend/about.html")
+    
